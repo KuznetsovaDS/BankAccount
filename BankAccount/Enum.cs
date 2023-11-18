97033bc15c71fdc030816223677e724732fba4f8
Darya Kuznetsova <Dashar1996@gmail.com>
2023-11-18 20:18:06 +0300

    Добавьте файлы проекта.

---
+++ BankAccount/Enum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+public enum AccountType { Checking, Deposit }
+class Program
+{
+    static void Main()
+    {
+        AccountType goldAccount;
+        AccountType platinumAccount;
+        goldAccount = AccountType.Checking;
+        platinumAccount = AccountType.Deposit;
+        Console.WriteLine("The Customer Account Type is {0}",
+goldAccount);
+        Console.WriteLine("The Customer Account Type is {0}",
+        platinumAccount);
+
+    }
+}
